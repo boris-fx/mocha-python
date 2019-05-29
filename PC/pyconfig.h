450d8bf777a15ae981e879bdecd85894813d76c1
Ivan Il'in <ivan.ilin@borisfx.com>
2019-05-29 18:10:59 +0100

    MO-9971: Migrate Imagineer's patch

--- PC/pyconfig.h
+++ PC/pyconfig.h
@@ -267,9 +267,7 @@ Py_NO_ENABLE_SHARED to find out.  Also support MS_NO_COREDLL for b/w compat */
                         /* So MSVC users need not specify the .lib
                         file in their Makefile (other compilers are
                         generally taken care of by distutils.) */
-#                       if defined(_DEBUG)
-#                               pragma comment(lib,"python310_d.lib")
-#                       elif defined(Py_LIMITED_API)
+#                       if defined(Py_LIMITED_API)
 #                               pragma comment(lib,"python3.lib")
 #                       else
 #                               pragma comment(lib,"python310.lib")
@@ -311,10 +309,6 @@ Py_NO_ENABLE_SHARED to find out.  Also support MS_NO_COREDLL for b/w compat */
 #       else
 #       define SIZEOF_TIME_T 4
 #       endif
-#endif
-
-#ifdef _DEBUG
-#       define Py_DEBUG
 #endif
 
 
