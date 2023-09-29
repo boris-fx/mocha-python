85c5ddffb87eb5417c69c90f747b9d8145663b98
mikw <mikw@imagineersystems.com>
2023-09-29 16:48:57 +0100

    MO-12256: Add support for a custom DYLD_LIBRARY_PATH
    Specifying a custom DYLD_LIBRARY_PATH allows you to build shared extensions such as _ssl and _hashlib
    as those extensions need to be able to find the opensll shared libraries when checking to
    see if the built extension can be successfully imported.

--- setup.py
+++ setup.py
@@ -1753,6 +1753,8 @@ class PyBuildExt(build_ext):
 
         # LZMA compression support.
         if self.compiler.find_library_file(self.lib_dirs, 'lzma'):
+            libfile = self.compiler.find_library_file(self.lib_dirs, 'lzma')
+            print(f'lzma: found {libfile}')
             self.add(Extension('_lzma', ['_lzmamodule.c'],
                                libraries=['lzma']))
         else:
